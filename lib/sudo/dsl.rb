8af6429c68261bb6344473c253211c1562e70701
Guido De Rosa <guido.derosa@vemarsas.it>
2010-10-23 18:37:28 +0200

    dsl: autostart

--- lib/sudo/dsl.rb
+++ lib/sudo/dsl.rb
@@ -3,6 +3,7 @@ require 'sudo/wrapper'
 module Sudo
 
   module DSL
+    
     def sudo_start(*args, &blk)
       @__default_sudo_wrapper = Sudo::Wrapper.new(*args, &blk).start!
     end
@@ -10,8 +11,13 @@ module Sudo
       @__default_sudo_wrapper.stop!
     end
     def sudo(object)
+      sudo_start unless sudo_started?
       @__default_sudo_wrapper[object]
     end
+    def sudo_started?
+      @__default_sudo_wrapper.running?
+    end
+
   end
 
 end
