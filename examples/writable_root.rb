2c2ea95bf5db80e7f0ccc824fc50b9b409995e5e
Guido De Rosa <guido.derosa@vemarsas.it>
2013-01-04 12:48:02 +0000

    add new examples/writable_root.rb

---
+++ examples/writable_root.rb
@@ -0,0 +1,30 @@
+require 'sudo'
+
+t0 = Time.now 
+su = Sudo::Wrapper.new.start!
+t1 =  Time.now
+
+writable_root                 = su[File].writable? '/'
+t2 = Time.now
+writable_root_by_normal_user  = File.writable? '/'
+
+puts
+puts "writable_root                                 = #{writable_root}"
+puts "writable_root_by_normal_user                  = #{writable_root_by_normal_user}"
+puts "Starting Sudo::Wrapper object took              #{t1-t0} seconds"
+puts "Using it, as in  su[File].writable? '/', took   #{t2-t1} seconds"
+
+puts
+puts "Hit ENTER to stop the Sudo::Wrapper object"
+puts "(in the meantime, you can look at the ruby sudo-ed process with ps)" 
+gets
+
+# Optional: it will be stopped automatically as soon as 'su' object gets out of scope
+su.stop!
+
+
+
+
+
+
+
