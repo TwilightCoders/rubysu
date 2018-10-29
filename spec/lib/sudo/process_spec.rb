8fcc57181fab9b83db56eebd4ffe48f1aa3d1be7
Dale Stevens <dale@getnotion.com>
2018-10-29 09:45:05 -0600

    Don't test this

--- spec/lib/sudo/process_spec.rb
+++ spec/lib/sudo/process_spec.rb
@@ -9,26 +9,4 @@ describe Process do
     expect(Process.exists?(-1)).to eq(false)
   end
 
-
-  it 'can kill what it wants' do
-    pid = fork do
-      Signal.trap("HUP") { puts "Oof!" }
-      Signal.trap("TERM") { puts "Ouch!" }
-      sleep 400
-       # ... do some work ...
-    end
-
-    ["HUP", "TERM", "KILL"].each do |sig|
-      puts "#{sig}ing..."
-      puts Process.kill(sig, pid)
-      # sleep 1
-      puts Process.exists?(pid)
-    end
-
-    # puts system "kill #{pid}"
-    # puts system "kill -9 #{pid}"
-
-    Process.wait
-  end
-
 end
