6427f10e50572cd1e17b000d384a978e59a88c30
Dale Stevens <dale@twilightcoders.net>
2025-07-23 12:33:51 -0600

    🚀 Modernize CI/CD infrastructure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter
+]
 
 SimpleCov.start do
   add_filter '/spec'
