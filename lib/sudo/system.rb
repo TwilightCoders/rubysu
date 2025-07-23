20d728c299759f8e52aedb0e5262002732800195
Dale Stevens <dale@twilightcoders.net>
2025-07-23 13:42:12 -0600

    🔒 Fix command injection vulnerabilities with secure command infrastructure
    - Add command_base() method for consistent secure command building
    - Add System.command() method to build secure array-form commands
    - Fix command injection in kill, unlink, and check methods using array-form system calls
    - Update wrapper spawn to use secure command arrays instead of string interpolation
    - Refactor command() and check() to share common base functionality
    - Update wrapper to handle environment variables properly
    - Eliminate all string interpolation in system calls to prevent injection attacks

--- lib/sudo/system.rb
+++ lib/sudo/system.rb
@@ -3,32 +3,48 @@ require 'sudo/constants'
 
 module Sudo
   module System
-
     ProcessStillExists  = Class.new(RuntimeError)
     FileStillExists     = Class.new(RuntimeError)
 
     class << self
-
       def kill(pid)
         if pid and Process.exists? pid
-          system "sudo kill     #{pid}" or
-          system "sudo kill -9  #{pid}" or
-          raise ProcessStillExists, "Couldn't kill sudo process (PID=#{pid})"
+          system("sudo", "kill", pid.to_s) or
+            system("sudo", "kill", "-9", pid.to_s) or
+            raise ProcessStillExists, "Couldn't kill sudo process (PID=#{pid})"
         end
+      end
+
+      def command(ruby_opts, socket, env = {})
+        cmd_args, env = command_base(env)
+        cmd_args << "-I#{LIBDIR}"
+        cmd_args.concat(ruby_opts.split) unless ruby_opts.empty?
+        cmd_args.concat([SERVER_SCRIPT.to_s, socket, Process.uid.to_s])
+        [cmd_args, env]
       end
 
       def unlink(file)
         if file and File.exist? file
-          system("sudo rm -f #{file}") or
-          raise(FileStillExists, "Couldn't delete #{file}")
+          system("sudo", "rm", "-f", file) or
+            raise(FileStillExists, "Couldn't delete #{file}")
         end
       end
 
       # just to check if we can sudo; and we'll receive a sudo token
       def check
-        raise SudoFailed unless system "#{SUDO_CMD} -E #{RUBY_CMD} -e ''"
+        cmd_args, env = command_base
+        cmd_args.concat(["-e", ""])
+        raise Sudo::Wrapper::SudoFailed unless system(env, *cmd_args)
       end
 
+      private
+
+      def command_base(env = {})
+        cmd_args = [SUDO_CMD]
+
+        cmd_args.concat(["-E", RUBY_CMD])
+        [cmd_args, env]
+      end
     end
   end
 end
