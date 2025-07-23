d59faf005c93e5afdc7e2d370386596fdbfc8fc2
Dale Stevens <dale@twilightcoders.net>
2025-07-23 14:37:05 -0600

    ✨ Add Sudo.as_root DSL convenience method
    Provide cleaner, more intuitive API for simple root operations.
    Delegates to Wrapper.run but with better semantic naming.

--- lib/sudo.rb
+++ lib/sudo.rb
@@ -1,3 +1,9 @@
 require 'sudo/configuration'
 require 'sudo/wrapper'
 
+module Sudo
+  # Convenience method for simple root operations
+  def self.as_root(**options, &block)
+    Wrapper.run(**options, &block)
+  end
+end
