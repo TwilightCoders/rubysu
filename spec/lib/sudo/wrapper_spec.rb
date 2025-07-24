5a17d2aadf4592e19108a9eb7d26f12375518a1b
Dale Stevens <dale@twilightcoders.net>
2025-07-24 12:39:54 -0600

    ✅ Add comprehensive test coverage for edge cases and error handling
    - Add MethodProxy specs for initialization, delegation, and respond_to_missing?
    - Add System module specs for kill, command, check, and command_base methods
    - Add Wrapper specs for error conditions, gem loading failures, and edge cases
    - Suppress warning output in test specs to clean up test runs
    - Improve coverage from 77% to 96%

--- spec/lib/sudo/wrapper_spec.rb
+++ spec/lib/sudo/wrapper_spec.rb
@@ -52,15 +52,15 @@ describe Sudo::Wrapper do
         wrapper_instance = double('wrapper', stop!: nil)
         allow(described_class).to receive(:new).and_return(wrapper_instance)
         allow(wrapper_instance).to receive(:start!).and_return(wrapper_instance)
-        
+
         expect(wrapper_instance).to receive(:stop!)
-        
+
         described_class.run { |sudo| }
       end
 
       it 'does not raise error when wrapper creation fails and sudo is nil' do
         allow(described_class).to receive(:new).and_raise(StandardError, "Creation failed")
-        
+
         # This should not raise NoMethodError due to safe navigation
         expect { described_class.run { |sudo| } }.to raise_error(StandardError, "Creation failed")
       end
@@ -69,9 +69,9 @@ describe Sudo::Wrapper do
         wrapper_instance = double('wrapper', stop!: nil)
         allow(described_class).to receive(:new).and_return(wrapper_instance)
         allow(wrapper_instance).to receive(:start!).and_return(wrapper_instance)
-        
+
         expect(wrapper_instance).to receive(:stop!)
-        
+
         expect do
           described_class.run { |sudo| raise "Block error" }
         end.to raise_error("Block error")
@@ -147,6 +147,168 @@ describe Sudo::Wrapper do
       it 'respects load_gems true configuration' do
         wrapper_with_gems = described_class.new(config: Sudo::Configuration.new(load_gems: true))
         expect(wrapper_with_gems.send(:load_gems?)).to eq(true)
+      end
+    end
+  end
+
+  describe 'Error handling and edge cases' do
+    describe '#start!' do
+      it 'raises error when socket creation times out' do
+        allow(Sudo::System).to receive(:check)
+        allow(Sudo::System).to receive(:command).and_return([['sudo', 'ruby'], {}])
+        allow_any_instance_of(Sudo::Wrapper).to receive(:spawn).and_return(1234)
+        allow(Process).to receive(:detach)
+        allow_any_instance_of(Sudo::Wrapper).to receive(:wait_for).and_return(false) # timeout
+
+        wrapper = described_class.new
+        expect { wrapper.start! }.to raise_error(RuntimeError, /Couldn't create DRb socket/)
+      end
+    end
+
+    describe '#running?' do
+      it 'returns false when process does not exist' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@sudo_pid, 99999)
+        wrapper.instance_variable_set(:@proxy, double('proxy'))
+        allow(wrapper).to receive(:socket?).and_return(true)
+        allow(Process).to receive(:exists?).with(99999).and_return(false)
+
+        expect(wrapper.running?).to be false
+      end
+
+      it 'returns false when socket does not exist' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@sudo_pid, 1234)
+        wrapper.instance_variable_set(:@proxy, double('proxy'))
+        allow(Process).to receive(:exists?).with(1234).and_return(true)
+        allow(wrapper).to receive(:socket?).and_return(false)
+
+        expect(wrapper.running?).to be false
+      end
+
+      it 'returns falsy when proxy is nil' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@sudo_pid, 1234)
+        wrapper.instance_variable_set(:@proxy, nil)
+        allow(Process).to receive(:exists?).with(1234).and_return(true)
+        allow(wrapper).to receive(:socket?).and_return(true)
+
+        expect(wrapper.running?).to be_falsy
+      end
+    end
+
+    describe 'gem loading error handling' do
+      it 'handles DRb marshaling errors in prospective_gems' do
+        wrapper = described_class.new
+        proxy = double('proxy')
+        wrapper.instance_variable_set(:@proxy, proxy)
+
+        allow(proxy).to receive(:loaded_specs).and_raise(StandardError.new('marshaling error'))
+        allow(wrapper).to receive(:warn) # Suppress warning output in tests
+        expect(wrapper.send(:prospective_gems)).to eq([])
+      end
+
+      it 'handles LoadError in try_gem_variants' do
+        wrapper = described_class.new
+        proxy = double('proxy')
+        wrapper.instance_variable_set(:@proxy, proxy)
+
+        allow(proxy).to receive(:proxy).with(Kernel, :require, 'test-gem').and_raise(LoadError)
+        allow(proxy).to receive(:proxy).with(Kernel, :require, 'test/gem').and_raise(LoadError)
+
+        # Should not raise error, just continue
+        expect { wrapper.send(:try_gem_variants, 'test-gem') }.not_to raise_error
+      end
+
+      it 'handles NameError in try_gem_variants' do
+        wrapper = described_class.new
+        proxy = double('proxy')
+        wrapper.instance_variable_set(:@proxy, proxy)
+
+        allow(proxy).to receive(:proxy).with(Kernel, :require, 'test-gem').and_raise(NameError)
+        allow(proxy).to receive(:proxy).with(Kernel, :require, 'test/gem').and_return(true)
+
+        expect { wrapper.send(:try_gem_variants, 'test-gem') }.not_to raise_error
+      end
+
+      it 'stops trying variants on successful require' do
+        wrapper = described_class.new
+        proxy = double('proxy')
+        wrapper.instance_variable_set(:@proxy, proxy)
+
+        expect(proxy).to receive(:proxy).with(Kernel, :require, 'test-gem').and_return(true).once
+        expect(proxy).not_to receive(:proxy).with(Kernel, :require, 'test/gem')
+
+        wrapper.send(:try_gem_variants, 'test-gem')
+      end
+    end
+
+    describe 'load_paths method' do
+      it 'adds missing paths from host to proxy' do
+        wrapper = described_class.new
+        proxy = double('proxy')
+        wrapper.instance_variable_set(:@proxy, proxy)
+
+        host_paths = ['/host/path1', '/host/path2', '/shared/path']
+        proxy_paths = ['/shared/path', '/proxy/path']
+
+        allow($LOAD_PATH).to receive(:-).and_return(['/host/path1', '/host/path2'])
+        allow(proxy).to receive(:load_path).and_return(proxy_paths)
+        allow(proxy).to receive(:add_load_path).with('/host/path1')
+        allow(proxy).to receive(:add_load_path).with('/host/path2')
+
+        expect(proxy).to receive(:add_load_path).with('/host/path1')
+        expect(proxy).to receive(:add_load_path).with('/host/path2')
+
+        wrapper.send(:load_paths)
+      end
+    end
+
+    describe 'Finalizer class' do
+      it 'calls cleanup when invoked' do
+        data = { pid: 1234, socket: '/tmp/test.sock' }
+        finalizer = described_class::Finalizer.new(data)
+
+        expect(described_class).to receive(:cleanup!).with(data)
+        finalizer.call
+      end
+    end
+
+    describe '.cleanup!' do
+      it 'calls System.kill and System.unlink' do
+        data = { pid: 1234, socket: '/tmp/test.sock' }
+
+        expect(Sudo::System).to receive(:kill).with(1234)
+        expect(Sudo::System).to receive(:unlink).with('/tmp/test.sock')
+
+        described_class.cleanup!(data)
+      end
+    end
+
+    describe '#server_uri' do
+      it 'returns drbunix URI with socket path' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@socket, '/tmp/test.sock')
+
+        expect(wrapper.server_uri).to eq('drbunix:/tmp/test.sock')
+      end
+    end
+
+    describe '#socket?' do
+      it 'returns true when socket file exists' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@socket, '/tmp/existing.sock')
+        allow(File).to receive(:exist?).with('/tmp/existing.sock').and_return(true)
+
+        expect(wrapper.socket?).to be true
+      end
+
+      it 'returns false when socket file does not exist' do
+        wrapper = described_class.new
+        wrapper.instance_variable_set(:@socket, '/tmp/missing.sock')
+        allow(File).to receive(:exist?).with('/tmp/missing.sock').and_return(false)
+
+        expect(wrapper.socket?).to be false
       end
     end
   end
